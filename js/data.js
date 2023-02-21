424918edd0f63740443698c1057436d536a15654
Ognjen Gavric <ognjengavric2@gmail.com>
2023-02-21 09:42:20 +0100

    End Bit Show Project

--- js/data.js
+++ js/data.js
@@ -1,59 +1,69 @@
-"use strict";
-const dataModule =(function() {
-  class TvShow {
-    constructor(id, name, rating, image) {
-      this.id = id;
-      this.name = name;
-      this.rating = rating;
-      this.image = image;
-    }
-  }
-
-  class DetailedShow extends TvShow {
-    constructor(id, name, image, castList, seasonsList, description) {
-      super(id, name, image);
-      this.castList = castList;
-      this.seasonsList = seasonsList;
-      this.description = description
-    }
-  }
-
+const dataModule = (function () {
   class Season {
     constructor(startDate, endDate) {
-      this.startDate = startDate,
-      this.endDate = endDate
+      this.startDate = startDate;
+      this.endDate = endDate;
+    }
+  }
+  class TvShow {
+    constructor(name, id, coverUrl, summary = '', cast = [], seasons = []) {
+      this.id = id;
+      this.name = name;
+      this.coverUrl = coverUrl;
+      this.summary = summary;
+      this.cast = cast;
+      this.seasons = seasons;
     }
   }
 
   const getShows = () => {
-    return fetch("https://api.tvmaze.com/show")
-      .then(response => response.json()) 
-      .then(shows => {
-        const allShows = [];
-        shows.map(show => {
-          const createdShow = new TvShow(show.id, show.name, show.rating.average, show.image.original);
-          allShows.push(createdShow);
-        })
-        allShows.sort((a,b) => b.rating - a.rating);
-        const top50 = allShows.slice(0,50);
-        return top50;
+    return fetch('http://api.tvmaze.com/shows')
+      .then(function (res) {
+        return res.json();
       })
-  }
+      .then(function (showsRawObjects) {
+        return showsRawObjects.map(
+          ({ name, id, image }) => new TvShow(name, id, image.original)
+        );
+      });
+  };
 
-  const searchShow = (input) => {
-    return fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
-      .then(response => response.json())
-      .then(data => data.slice(0, 10).map(({ show }) => {
+  const getSingleTvShow = (id) => {
+    return fetch(
+      `http://api.tvmaze.com/shows/${id}?embed[]=seasons&embed[]=cast`
+    )
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (rawTvShow) {
+        const tvSeasons = rawTvShow._embedded.seasons.map(
+          (s) => new Season(s.premiereDate, s.endDate)
+        );
+        const cast = rawTvShow._embedded.cast.map((a) => a.person.name);
+        return new TvShow(
+          rawTvShow.name,
+          rawTvShow.id,
+          rawTvShow.image.original,
+          rawTvShow.summary,
+          cast,
+          tvSeasons
+        );
+      });
+  };
+
+  const searchShow = (term) => {
+    return fetch(`https://api.tvmaze.com/search/shows?q=${term}`)
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (showsRawObjects) {
+        return showsRawObjects.map(({ show }) => {
           const { name, id, image } = show;
-          const imageSearch = image ? image.original : "";
-          return new TvShow(name, id, imageSearch);
-        })
-      )
-  }
+          const imageToUse = image ? image.original : '';
+          return new TvShow(name, id, imageToUse);
+        });
+      });
+  };
 
-  return {
-    getShows,
-    searchShow
-  }
-
-})()
+  return { getShows, searchShow, getSingleTvShow };
+})();
